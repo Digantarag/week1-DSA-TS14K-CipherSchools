53e429e60581edb100d66cffe3530c2d731cf82b
Digantarag <74209122+Digantarag@users.noreply.github.com>
2023-07-02 15:27:20 +0530

    Add files via upload

---
+++ Lecture1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void mergeArray(vector<int> &a, vector<int> &b, int m, int n){
+    int i = m-1 , j = n-1, k = m+n-1;
+    while(i >= 0 or j >= 0){
+        if (a[i] >= b[j]){
+            a[k] = a[i];
+            i--;
+            k--;
+        } else {
+            a[k] = b[j];
+            j--;
+            k--;
+        }
+    }
+    while (j >= 0) {
+        a[k] = b[j];
+        j--;
+        k--;
+    }
+}
+
+int main() {
+    return 0;
+}
